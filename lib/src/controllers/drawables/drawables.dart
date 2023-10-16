79545f1a7f3925417554bdcbe864de1bf02ae60c
Lukas Knejp <knejp.lukas41@gmail.com>
2023-10-16 13:18:48 +0200

    grouped export

--- lib/src/controllers/drawables/drawables.dart
+++ lib/src/controllers/drawables/drawables.dart
@@ -1,7 +1,8 @@
-export 'drawable.dart';
-export 'path/path_drawables.dart';
-export 'text_drawable.dart';
-export 'object_drawable.dart';
 export 'background/background_drawables.dart';
-export 'shape/shape_drawables.dart';
+export 'drawable.dart';
+export 'grouped_drawable.dart';
 export 'image_drawable.dart';
+export 'object_drawable.dart';
+export 'path/path_drawables.dart';
+export 'shape/shape_drawables.dart';
+export 'text_drawable.dart';
